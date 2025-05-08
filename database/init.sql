641adea1a58c376f333229f965fdfb7ea9a0d14e
adam <fittleradam@gmail.com>
2025-05-08 11:03:44 +1000

    setup the postgress init file

---
+++ database/init.sql
@@ -0,0 +1,18 @@
+-- create role table
+CREATE TABLE IF NOT EXISTS USERS (
+    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE IF NOT EXISTS CHAT (
+    chat_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid,
+    history TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+);
